0309ff05904c52df2057ed416b9e200126dc3a4f
J <jeremy.w.wilder@gmail.com>
2017-09-24 22:39:01 -0400

    Style changes

--- app/src/main/java/odomobileapplicationdevelopment/hikingapp/MainActivity.java
+++ app/src/main/java/odomobileapplicationdevelopment/hikingapp/MainActivity.java
@@ -1,7 +1,9 @@
 package odomobileapplicationdevelopment.hikingapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void goCamping(View view){
+        Toast.makeText(this,"LETS GO CAMPING!",Toast.LENGTH_SHORT).show();
+    }
+
+    public void goHiking(View view){
+        Toast.makeText(this,"LETS GO HIKING!",Toast.LENGTH_SHORT).show();
+    }
+
+    public void goSwimming(View view){
+        Toast.makeText(this,"LETS GO SWIMMING!",Toast.LENGTH_SHORT).show();
     }
 }
